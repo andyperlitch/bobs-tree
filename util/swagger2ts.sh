30a42498b248ab33fc5231e457478ee613f00fcc
Andrew Perlitch <perlitch@cisco.com>
2016-12-21 19:21:59 -0800

    updates to swagger2ts script

--- util/swagger2ts.sh
+++ util/swagger2ts.sh
@@ -1,7 +1,16 @@
+set -e
 TMP_LOCATION=client/.tmp/generated-api
 TRG_LOCATION=client/src/app/services/DefaultApi
+GENERATOR=typescript-angular
+GENERATOR_TARGET_DIR=API/Client
+# GENERATOR=typescript-angular2
+# GENERATOR_TARGET_DIR=api
 
-swagger-codegen generate -i api/swagger/swagger.yaml -l typescript-angular -o "${TMP_LOCATION}"
-
-cp $TMP_LOCATION/API/Client/* $TRG_LOCATION/
+echo "swagger2ts.sh: using the '${GENERATOR}' generator to create client files."
 rm -rf $TMP_LOCATION
+mkdir -p $TMP_LOCATION
+swagger-codegen generate -i api/swagger/swagger.yaml -l $GENERATOR -o "${TMP_LOCATION}"
+mkdir -p $TRG_LOCATION
+cp $TMP_LOCATION/$GENERATOR_TARGET_DIR/* $TRG_LOCATION/
+rm -rf $TMP_LOCATION
+echo "swagger2ts.sh: success!"
